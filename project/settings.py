f5c669f556a89c7727635f103d97903f59593a57
ErJun Wu <defyto13579@gmail.com>
2024-09-26 11:25:06 +0800

    fix: set heroku mysql db

--- project/settings.py
+++ project/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+
 import os
 
 load_dotenv()
@@ -138,15 +140,20 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.getenv("DB_ENGINE"),
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('JAWSDB_URL')
+    )
 }
 
 ENCRYPTION_KEY = {
