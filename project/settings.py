f73e4a3e2955325f88fd6f99c9fea010771c8125
ErJun Wu <defyto13579@gmail.com>
2024-09-27 17:25:44 +0800

    update

--- project/settings.py
+++ project/settings.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
-
+import pymysql
 import os
 
 load_dotenv()
 
 from django.contrib.messages import constants as messages
-
+pymysql.install_as_MySQLdb()
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
@@ -140,22 +140,22 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("DB_ENGINE"),
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('JAWSDB_URL')
-    )
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('JAWSDB_URL')
+#     )
+# }
 
 ENCRYPTION_KEY = {
     "MERCHANT_ID": os.getenv("MERCHANTID"),
