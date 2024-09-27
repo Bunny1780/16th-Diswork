9f7ab61aff0535405292dfc24c81516b9334815b
ErJun Wu <defyto13579@gmail.com>
2024-09-27 21:38:51 +0800

    update

--- project/settings.py
+++ project/settings.py
@@ -94,6 +94,8 @@ SOCIALACCOUNT_PROVIDERS = {
     },
 }
 
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = os.getenv('ACCOUNT_DEFAULT_HTTP_PROTOCOL', 'http')
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
